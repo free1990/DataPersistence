2c101118d53375fca3eef8c2a7d1a76e50c7a793
free1990 <free1990@163.com>
2015-04-03 16:44:52 +0800

    Add file save helper method

--- DataPersistence/ViewController.m
+++ DataPersistence/ViewController.m
@@ -127,6 +127,153 @@
     NSLog(@"self.imageView.image = %@ width = %f, height = %f , malloc size = %ld", self.imageView.image, self.imageView.image.size.width, self.imageView.image.size.height, sizeof(self.imageView.image));
     
     NSLog(@"%@||%ld||%@",name,age,address);
+    
+    [self listAllLocalFiles];
+}
+
+//list all the files exists in Document Folder in our Sandbox.
+- (void)listAllLocalFiles
+{
+    // Fetch directory path of document for local application.
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    
+    // NSFileManager is the manager organize all the files on device.
+    NSFileManager *manager = [NSFileManager defaultManager];
+    // This function will return all of the files' Name as an array of NSString.
+    NSArray *files = [manager contentsOfDirectoryAtPath:documentsDirectory error:nil];
+    // Log the Path of document directory.
+    NSLog(@"Directory: %@", documentsDirectory);
+    // For each file, log the name of it.
+    for (NSString *file in files) {
+        NSLog(@"File at: %@", file);
+    }
+}
+
+//Create a File in the Document Folder.
+- (void)createFileWithName:(NSString *)fileName
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:fileName];
+    
+    NSFileManager *manager = [NSFileManager defaultManager];
+    // 1st, This funcion could allow you to create a file with initial contents.
+    // 2nd, You could specify the attributes of values for the owner, group, and permissions.
+    // Here we use nil, which means we use default values for these attibutes.
+    // 3rd, it will return YES if NSFileManager create it successfully or it exists already.
+    if ([manager createFileAtPath:filePath contents:nil attributes:nil]) {
+        NSLog(@"Created the File Successfully.");
+    } else {
+        NSLog(@"Failed to Create the File");
+    }
+}
+
+//Delete a File in the Document Folder.
+- (void)deleteFileWithName:(NSString *)fileName
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    // Have the absolute path of file named fileName by joining the document path with fileName, separated by path separator.
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:fileName];
+    
+    NSFileManager *manager = [NSFileManager defaultManager];
+    // Need to check if the to be deleted file exists.
+    if ([manager fileExistsAtPath:filePath]) {
+        NSError *error = nil;
+        // This function also returnsYES if the item was removed successfully or if path was nil.
+        // Returns NO if an error occurred.
+        [manager removeItemAtPath:filePath error:&error];
+        if (error) {
+            NSLog(@"There is an Error: %@", error);
+        }
+    } else {
+        NSLog(@"File %@ doesn't exists", fileName);
+    }
+}
+
+//Rename a File in the Document Folder.
+- (void)renameFileWithName:(NSString *)srcName toName:(NSString *)dstName
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *filePathSrc = [documentsDirectory stringByAppendingPathComponent:srcName];
+    NSString *filePathDst = [documentsDirectory stringByAppendingPathComponent:dstName];
+    NSFileManager *manager = [NSFileManager defaultManager];
+    if ([manager fileExistsAtPath:filePathSrc]) {
+        NSError *error = nil;
+        [manager moveItemAtPath:filePathSrc toPath:filePathDst error:&error];
+        if (error) {
+            NSLog(@"There is an Error: %@", error);
+        }
+    } else {
+        NSLog(@"File %@ doesn't exists", srcName);
+    }
+}
+
+
+//Read a File in the Document Folder.
+/* This function read content from the file named fileName.
+ */
+- (void)readFileWithName:(NSString *)fileName
+{
+    // Fetch directory path of document for local application.
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    // Have the absolute path of file named fileName by joining the document path with fileName, separated by path separator.
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:fileName];
+    
+    // NSFileManager is the manager organize all the files on device.
+    NSFileManager *manager = [NSFileManager defaultManager];
+    if ([manager fileExistsAtPath:filePath]) {
+        // Start to Read.
+        NSError *error = nil;
+        NSString *content = [NSString stringWithContentsOfFile:filePath encoding:NSStringEncodingConversionAllowLossy error:&error];
+        NSLog(@"File Content: %@", content);
+        
+        if (error) {
+            NSLog(@"There is an Error: %@", error);
+        }
+    } else {
+        NSLog(@"File %@ doesn't exists", fileName);
+    }
+}
+
+//Write a File in the Document Folder.
+/* This function Write "content" to the file named fileName.
+ */
+- (void)writeString:(NSString *)content toFile:(NSString *)fileName
+{
+    // Fetch directory path of document for local application.
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    // Have the absolute path of file named fileName by joining the document path with fileName, separated by path separator.
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:fileName];
+    
+    // NSFileManager is the manager organize all the files on device.
+    NSFileManager *manager = [NSFileManager defaultManager];
+    // Check if the file named fileName exists.
+    if ([manager fileExistsAtPath:filePath]) {
+        NSError *error = nil;
+        // Since [writeToFile: atomically: encoding: error:] will overwrite all the existing contents in the file, you could keep the content temperatorily, then append content to it, and assign it back to content.
+        // To use it, simply uncomment it.
+        //        NSString *tmp = [[NSString alloc] initWithContentsOfFile:fileName usedEncoding:NSStringEncodingConversionAllowLossy error:nil];
+        //        if (tmp) {
+        //            content = [tmp stringByAppendingString:content];
+        //        }
+        // Write NSString content to the file.
+        [content writeToFile:filePath atomically:YES encoding:NSStringEncodingConversionAllowLossy error:&error];
+        // If error happens, log it.
+        if (error) {
+            NSLog(@"There is an Error: %@", error);
+        }
+    } else {
+        // If the file doesn't exists, log it.
+        NSLog(@"File %@ doesn't exists", fileName);
+    }
+    
+    // This function could also be written without NSFileManager checking on the existence of file,
+    // since the system will atomatically create it for you if it doesn't exist.
 }
 
 - (void)didReceiveMemoryWarning {
